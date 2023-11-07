42d762a2cb55f1b690cdc2223b411f3d664a9fcb
Nunoc99 <nunocunha99@ua.pt>
2023-11-07 11:22:11 +0000

    kuku

---
+++ psr22/Parte_09/psr_parte09_ex1/src/subscriber.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import String
+
+def msgReceivedCallback(msg):
+    rospy.loginfo("I received " + str(msg.data))
+    
+def main():
+
+    rospy.init_node('subscriber', anonymous=True)
+
+    rospy.Subscriber("chatter", String, msgReceivedCallback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
